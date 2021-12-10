d944c86ed72bdc3174ae6f7fee4bd1e067cd99b4
Laurie Lonecrow <laurieteresa@gmail.com>
2021-12-10 17:01:04 -0500

    fix: font-size card title

--- src/components/HowSection/Card/index.js
+++ src/components/HowSection/Card/index.js
@@ -7,10 +7,9 @@ export default function Card({stepNumber, title, body}) {
     return (
         <div className={styles.card_wrapper}>
           <div className={styles.number}>
-            0{stepNumber}
+            0{stepNumber} 
             <div className={styles.divider}></div>
           </div>
-
           <div className={styles.card_title}>
             {title}
           </div>
